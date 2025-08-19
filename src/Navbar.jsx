db588846d02d9f06fc47ce41446f5ce982cd71ab
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-19 07:39:03 -0700

    NavBar structure done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div>
+            <nav>
+                <div className='logo'>
+                    <h1>Alpha Tech</h1>
+
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Sign Up</li>
+                </ul>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
