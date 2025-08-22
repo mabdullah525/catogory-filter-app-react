8dea511906d373f83e31a69b448488b70889f48d
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-22 10:33:42 -0700

    Implement category selection and sorting functionality in Category component

--- src/Category.jsx
+++ src/Category.jsx
@@ -4,12 +4,41 @@ import { AllData } from './Data'
 
 const Category = () => {
     const [data, setData] = useState(AllData);
+    const [inputVal, setInputVal] = useState("");
+
+    const selectIteam = (e) => {
+        console.log(e.target.value);
+        setInputVal(e.target.value);
+    }
+    if (inputVal === "Product A to Z") {
+        data.sort((a, b) => {
+            let nameA = a.name;
+            let nameB = b.name;
+
+            if (nameA < nameB) {
+                return -1;
+            }
+
+        })
+    }
+      if (inputVal === "Product Z to A") {
+        data.sort((a, b) => {
+            let nameA = a.name;
+            let nameB = b.name;
+
+            if (nameA < nameB) {
+                return -1;
+            }
+
+        })
+    }
+
     return (
         <div className='app-wrapper'>
             <Navbar />
             <div className="category">
                 <label htmlFor="select">Category :</label>
-                <select id="select" className="select-box">
+                <select id="select" className="select-box" onChange={selectIteam}>
                     <option value="Product A to Z">Product A to Z</option>
                     <option value="Product Z to A">Product Z to A</option>
                     <option value="Price High to Low">Price High to Low</option>
